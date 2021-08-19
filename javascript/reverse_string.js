72239655f90a52e6c5b2ebc71017ccefdf90e33e
jennafritz <jennafritz24@gmail.com>
2021-08-19 12:53:53 -0400

    Completed JS

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,14 @@
 function reverseString(str) {
   // type your code here
+  if(str.length === 0){
+    return str
+  }
+
+  if(str.length === 1){
+    return str[0]
+  }
+
+  return str[str.length - 1] + reverseString(str.substring(0, str.length - 1))
 }
 
 if (require.main === module) {
@@ -7,7 +16,8 @@ if (require.main === module) {
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString('ih'));
 
-  console.log("");
+  console.log("Expecting: 'hello'");
+  console.log("=>", reverseString('olleh'));
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString('catbaby'));
@@ -16,4 +26,8 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+  // if string length is 1
+    // return first (only) element
+  // return the last element + the return value of calling the function again with the last element removed from the string
+
 // And a written explanation of your solution
